1f1769c57af861bb77f863e2448912d204d35b76
exenza <massimo.berta@gmail.com>
2016-03-12 20:19:55 +0000

    changed message

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ MongoClient.connect(mongoURL, function(err, db) {
 
 console.log('App get')
 app.get("/", function(req, res){
-      res.send("All good");
+      res.send("All really good");
     })
     
 app.listen(process.env.PORT || 8080)
